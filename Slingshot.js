2a954cf91f3141b042e75c39f6529b2d3e885689
saloni1966 <salonisharma1966@gmail.com>
2021-01-17 17:37:20 +0530

    first commit

---
+++ Slingshot.js
@@ -0,0 +1,33 @@
+class SlingShot{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.04,
+            length: 10
+        }
+        this.sling1 = loadImage("sprites/sling1.png");
+        this.sling2 = loadImage("sprites/sling2.png");
+        this.sling3 = loadImage("sprites/sling3.png");
+        this.pointB = pointB
+        this.sling = Constraint.create(options);
+        World.add(world, this.sling);
+    }
+
+    fly(){
+        this.sling.bodyA = null;
+    }
+
+    display(){
+        image(this.sling1,200,20);
+        image(this.sling2,170,15);
+        if(this.sling.bodyA){
+            var pointA = this.sling.bodyA.position;
+            var pointB = this.pointB;
+            strokeWeight(4);
+            stroke(84, 39, 16)
+            line(pointA.x-20, pointA.y, pointB.x-20, pointB.y);
+        }
+    }
+    
+}
