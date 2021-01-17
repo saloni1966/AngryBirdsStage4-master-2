bf73a01ae7b034962e9255e3ebadd4e55541dcb1
saloni1966 <salonisharma1966@gmail.com>
2021-01-17 18:03:19 +0530

    "sling"

--- Slingshot.js
+++ Slingshot.js
@@ -21,12 +21,28 @@ class SlingShot{
     display(){
         image(this.sling1,200,20);
         image(this.sling2,170,15);
+      
         if(this.sling.bodyA){
             var pointA = this.sling.bodyA.position;
             var pointB = this.pointB;
-            strokeWeight(4);
-            stroke(84, 39, 16)
-            line(pointA.x-20, pointA.y, pointB.x-20, pointB.y);
+            stroke(84, 39, 16);
+
+            if(pointA.x < 220){
+                strokeWeight(6);
+            
+                line(pointA.x-20, pointA.y, pointB.x-20, pointB.y);
+                line(pointA.x-20, pointA.y, pointB.x+20, pointB.y);
+                image(this.sling3,pointA.x-30, pointA.y-10,15,30);
+            }
+            else{
+                strokeWeight(4);
+            
+                line(pointA.x+20, pointA.y, pointB.x-20, pointB.y);
+                line(pointA.x+20, pointA.y, pointB.x+20, pointB.y);
+                image(this.sling3,pointA.x+20, pointA.y-10,15,30);
+
+            }
+           
         }
     }
     
